458dba70a72c97610b0b9de1ef655cfbd020755e
Stanislav Baiduzhyi <Baiduzhyi@gmail.com>
2014-03-22 19:49:57 +0200

    fixing execution from current shell

--- setupQuiltEnv.sh
+++ setupQuiltEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QUILT_PATCHES=$(realpath $(dirname $0)/quilt-patches)
+QUILT_PATCHES=$(realpath $(dirname ${BASH_SOURCE[0]})/quilt-patches)
 echo "setting QUILT_PATCHES variable to: $QUILT_PATCHES"
 export QUILT_PATCHES
 
